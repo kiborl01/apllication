6553fed0473282766870b3455610dfd540c685a4
Dmitriy Dmitrov <kibrol@icloud.com>
2023-04-14 20:54:12 +0300

    maxima_HW_5

--- src/lesson_5/HW_5.java
+++ src/lesson_5/HW_5.java
@@ -99,9 +99,29 @@ public class HW_5 {
         System.out.println("Задача 7");
         System.out.println("Задача 7 в мейне HW_5_Task7");
 
-
+        Scanner sc7 = new Scanner(System.in);;
+        int[] array7 = new int[20];
+        for (int i = 0; i < 20; i++) {
+            array7[i] = sc7.nextInt();
+        }
+        for (int x : array7) {
+            System.out.println(x);
+        }
+    }
+    public static void bubbleSort(int[] array7) {
+        for (int i = 0; i < array7.length; i++)
+            for (int j = i + 1; j < array7.length; j++)
+                if (array7[j] > array7[i]) {
+                    int sortirovka = array7[i];
+                    array7[i] = array7[j];
+                    array7[j] = sortirovka;
+                }
     }
 }
+
+
+
+
 
 
 
